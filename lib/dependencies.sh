b857956d46f6e73c6c514899537a0008ae8f4201
Andreia Gaita <shana@github.com>
2018-08-23 13:38:35 +0200

    Prefer local cached packages when installing with yarn
    When using yarn to install dependencies, it should prefer a local cache as the source of packages, if one exists, so it doesn't download packages every time even though they must be checked in to the repository.

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -91,7 +91,7 @@ yarn_node_modules() {
 
   echo "Installing node modules (yarn.lock)"
   cd "$build_dir"
-  yarn install --production=$production --frozen-lockfile --ignore-engines 2>&1
+  yarn install --production=$production --frozen-lockfile --ignore-engines --prefer-offline 2>&1
 }
 
 yarn_prune_devdependencies() {
